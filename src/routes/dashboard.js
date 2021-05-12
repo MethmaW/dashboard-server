30bc8932b85330df6393b3790a723f3dcd403020
MethmaW <methma.cw@gmail.com>
2021-05-12 15:54:13 +0530

    Change fetching logic

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,101 +1,43 @@
 const router = require("express").Router();
 const fetch = require("node-fetch");
-const redis = require("redis");
-
-//creating redis client
-const client = redis.createClient({
-	port: process.env.REDIS_PORT,
-	host: process.env.REDIS_HOST,
-	password: process.env.REDIS_PASSWORD,
-});
-
-//getting chached equipemnt data
-const getChacheData = (req, res, next) => {
-	const { max, last } = req.params;
-
-	//checking if the max value is equal to cached max value
-	(() => {
-		client.get("max", (err, data) => {
-			if (err) throw err;
-			if (data == null) next();
-			if (data !== max) next();
-
-			if (data === max) {
-				getLastValue();
-			}
-		});
-	})();
-
-	//checking if the last value is equal to cached last value
-	const getLastValue = () => {
-		client.get("last", (err, data) => {
-			if (err) throw err;
-			if (data == null) next();
-			if (data !== last) next();
-
-			if (data === last) {
-				getAllEquipemnts();
-			}
-		});
-	};
-
-	//getting equipment data if chached max value and last value are equal to current max and last value
-	const getAllEquipemnts = () => {
-		client.get(`allEquipments`, (err, data) => {
-			if (err) throw err;
-			if (data == null) next();
-			if (data !== null) {
-				const par = JSON.parse(data);
-				res.json({
-					success: true,
-					msg: "Fetched cached data successfully!",
-					data: par,
-				});
-			}
-		});
-	};
-};
 
 const getEquipments = async (req, res) => {
 	try {
-		const { max, last } = req.params;
+		const max = 100;
+		let last = 0;
+		let isNotEmpty = true;
+		let data = [];
 
-		//fetching equipment data from the external api
-		const response = await fetch(
-			`http://ivivaanywhere.ivivacloud.com/api/Asset/Asset/All?apikey=${process.env.API_KEY}&max=${max}&last=${last}`,
-			{
-				method: "GET",
-				headers: { "Content-Type": "application/json" },
+		while (isNotEmpty) {
+			//fetching equipment data from the external api
+			const response = await fetch(
+				`http://ivivaanywhere.ivivacloud.com/api/Asset/Asset/All?apikey=${process.env.API_KEY}&max=${max}&last=${last}`,
+				{
+					method: "GET",
+					headers: { "Content-Type": "application/json" },
+				}
+			);
+
+			const responseData = await response.json();
+			isNotEmpty = responseData.length !== 0;
+
+			if (isNotEmpty) {
+				data = data.concat(responseData);
+				last = responseData[responseData.length - 1].__rowid__;
 			}
-		);
 
-		const data = await response.json();
-		const redisValue = JSON.stringify(data);
-
-		//setting max value in redis
-		client.set(`max`, max, function (err, reply) {
-			if (err) throw err;
-			//console.log("Saved max status: ", reply);
-		});
-
-		//setting last value in redis
-		client.set(`last`, last, function (err, reply) {
-			if (err) throw err;
-			//console.log("Saved last status: ", reply);
-		});
-
-		//setting equipment data in redis
-		client.set(`allEquipments`, redisValue, function (err, reply) {
-			if (err) throw err;
-			//console.log("Saved data status: ", reply);
-		});
+			console.log("last", last);
+			console.log("isNotEmpty", isNotEmpty);
+		}
 
 		res.json({
 			success: true,
 			msg: "Data fetched successfully!",
+			length: data.length,
 			data: data,
 		});
 	} catch (error) {
+		console.log(error);
 		res.json({
 			success: false,
 			msg: "Server error, Please contact support",
@@ -104,6 +46,6 @@ const getEquipments = async (req, res) => {
 	}
 };
 
-router.get("/:max/:last", getChacheData, getEquipments);
+router.get("/", getEquipments);
 
 module.exports = router;
